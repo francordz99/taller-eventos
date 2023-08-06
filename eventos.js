f7cda91e04e173e9a6795902c83548b5806ac20a
Franco Rodriguez <francordz32@gmail.com>
2023-08-06 08:23:16 -0300

    Agregada funcionalidad al div

---
+++ eventos.js
@@ -0,0 +1,7 @@
+// Obtener referencia al div por su ID
+const contenedor = document.getElementById("contenedor");
+
+// Agregar el manejador de evento al div
+contenedor.addEventListener("click", function () {
+    alert('Hola! Soy el div');
+});
