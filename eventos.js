cf6091a9302eef95f2f734b2aa2dd78956d0e9d6
Franco Rodriguez <francordz32@gmail.com>
2023-08-06 08:45:09 -0300

    Agregada solucion al doble cartel

--- eventos.js
+++ eventos.js
@@ -1,7 +1,15 @@
-// Obtener referencia al div por su ID
+
 const contenedor = document.getElementById("contenedor");
 
-// Agregar el manejador de evento al div
+
 contenedor.addEventListener("click", function () {
     alert('Hola! Soy el div');
+});
+
+const saludarBoton = document.getElementById("saludarBoton");
+
+saludarBoton.addEventListener("click", function () {
+    event.stopPropagation();
+    alert('Hola!');
+
 });
